c2a78976a6c5671e866a2141c07e44dfd60cd90e
Kas S <82743976+Kas-S@users.noreply.github.com>
2021-11-16 23:28:23 +0500

    Update settings.py

--- email_sender/email_sender/settings.py
+++ email_sender/email_sender/settings.py
@@ -129,5 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'codekoala7@gmail.com'
-EMAIL_HOST_PASSWORD = 'locgjotqfrdugznl'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
